6b9c5d249709e03884ba8b58598b53d339cdb8dc
rehanvdm <rehan.vdm4@gmail.com>
2024-07-04 06:19:09 +0200

    cleanup from fault finding

--- .projenrc.ts
+++ .projenrc.ts
@@ -41,10 +41,10 @@ const project = new awscdk.AwsCdkConstructLibrary({
   /* Runtime dependencies of this module that are jsii-enabled. */
   // deps: [ ],
   /*  Runtime dependencies of this module that are NOT jsii-enabled. */
-  bundledDeps: ['execa@5.1.1', '@aws-sdk/client-sts', '@aws-sdk/credential-providers', '@aws-sdk/client-cost-explorer', 'table', 'husky'],
+  bundledDeps: ['execa@5.1.1', '@aws-sdk/client-sts', '@aws-sdk/credential-providers', '@aws-sdk/client-cost-explorer', 'table'],
   // description: undefined,
   /* Build dependencies for this repo/module. */
-  // devDeps: ['husky'], //TODO: Just test
+  devDeps: ['husky'],
   /* The "name" in package.json. */
   // packageName: undefined,
   jestOptions: {
@@ -60,13 +60,11 @@ project.package.addEngine('npm', '~10.*');
 
 // Need to clear before compiling and packaging. Have to remove these because they are not cleared for some reason,
 // only new files are added and it causes issues especially because when changing the folder structure the whole time.
-// const clear = project.addTask('clear-lib-and-dist');
-// clear.exec('rm -rf lib/ dist/');
+const clear = project.addTask('clear-lib-and-dist');
+clear.exec('rm -rf lib/ dist/');
 
+// Only run husky if not in CI, in the post install script
 project.package.setScript('prepare', 'if [ "$CI" = "true" ]; then echo "CI detected, not running husky"; else husky; fi');
-const buildWorkflow = project.tryFindObjectFile('.github/workflows/build.yml');
-buildWorkflow?.addOverride('jobs.package-js.env.CI', 'true');
-// buildWorkflow?.addOverride('jobs.package-js.steps.5.run', 'cd .repo && npm install');
 
 project.gitignore.addPatterns('.dlz-reports');
 project.gitignore.addPatterns('.idea');
