35160f392849a86a80616c85324c010c3a3a8801
rehanvdm <rehan.vdm4@gmail.com>
2024-11-28 17:42:15 +0200

    ci: fix build log error

--- docs/cloudflare_build.sh
+++ docs/cloudflare_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# TODO: See how we can optimize this, probably ship the headless browser for OS with
 npx playwright install
-npx playwright install-deps
+# npx playwright install-deps Cloudflare complains in build logs but it works fine without this
 
 if [ "$CF_PAGES_BRANCH" = "main" ]; then
   npm run "build:production"
