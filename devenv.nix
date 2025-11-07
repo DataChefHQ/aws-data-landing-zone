2dbd078a29792ca0a0cc2cda629eb016c5a2af9b
Reza (Shahin) Khanipour <Shahin@DataChef.co>
2025-11-07 12:48:11 +0100

    feat: use devenv to manage developer environment.

---
+++ devenv.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [];
+
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+  languages.typescript.enable = true;
+  languages.javascript = {
+    enable = true;
+    npm = {
+      enable = true;
+      install.enable = true;
+    };
+  };
+
+  git-hooks.hooks = {
+    alejandra = {
+      enable = true;
+      settings.exclude = [".devenv.flake.nix"];
+    };
+    shellcheck.enable = true;
+    eslint.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
